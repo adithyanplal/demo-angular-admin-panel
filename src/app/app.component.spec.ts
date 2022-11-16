ee277893f3ac9ad269023ec699bc86a4c8accf7d
adithyanplal <adithyainfo811@gmail.com>
2022-11-16 14:38:42 +0530

    Demo without Real API

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'flash-grocy-admin'`, () => {
+  it(`should have as title 'demo-admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('flash-grocy-admin');
+    expect(app.title).toEqual('demo-admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('flash-grocy-admin app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demo-admin app is running!');
   });
 });
